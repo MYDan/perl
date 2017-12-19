c442cc363edd432c7c090bfeb0ab614eb283bc6b
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-19 18:50:34 +0800

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-
 OS=$(uname)
-BU='https://raw.githubusercontent.com/MYDan/perl/master'
-BP='/opt/mydan'
+PERLURL='https://raw.githubusercontent.com/MYDan/perl/master'
+INSTALLERDIR='/opt/mydan'
 
-if [ -f $BP/perl/.lock ]; then
+
+for T in "Linux"
+do
+    [ "X$SO" == "X$T" ] &&  break
+done
+
+if [ "X$OS" != "X$T" ]; then
+  echo "$OS : Not supported"
+  exit 1
+fi
+
+if [ -f $INSTALLERDIR/perl/.lock ]; then
     echo "The perl is locked"
-    exit;
+    exit 1;
 fi
 
-if [ -d "$BP/perl" ]; then
+checktool() {
+    if ! type $1 >/dev/null 2>&1; then
+        echo "Need tool: $1"
+        exit 1;  
+    fi
+}
+
+checktool curl
+checktool wget
+
+if [ -d "$INSTALLERDIR/perl" ]; then
     echo 'Already installed'
-    exit  
+    exit 1
 fi
 
-version=$(curl -s $BU/data/$OS/version)
-
+version=$(curl -s $PERLURL/data/$OS/version)
 if [[ $version =~ ^[0-9]{14}$ ]];then
     echo "version: $version"
 else
@@ -23,13 +42,23 @@ else
     exit;
 fi
 
-wget -O perl.$version.tar.gz $BU/data/$OS/perl.$version.tar.gz
 
-if [ ! -e $BP ]; then
-    mkdir -p $BP
+clean_exit () {
+    [ -f $LOCALINSTALLER ] && rm $LOCALINSTALLER
+    echo  "ERROR"
+    exit $1
+}
+
+LOCALINSTALLER=$(mktemp perl.XXXXXX)
+
+wget -O $LOCALINSTALLER $PERLURL/data/$OS/perl.$version.tar.gz || clean_exit 1
+
+if [ ! -e $INSTALLERDIR ]; then
+    mkdir -p $INSTALLERDIR
 fi
 
-tar -zxvf perl.$version.tar.gz -C $BP
-rm -f perl.$version.tar.gz
+tar -zxf $LOCALINSTALLER -C $INSTALLERDIR || clean_exit 1
+
+[ -f $LOCALINSTALLER ] && rm $LOCALINSTALLER
 
 echo OK
