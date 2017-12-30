6d84ba54da5405a31de2e1f71da8d1b84b589d38
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-30 13:57:31 +0800

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,78 +1,71 @@
 #!/bin/bash
 OS=$(uname)
 ARCH=$(uname -m)
+checktool() {
+    if ! type $1 >/dev/null 2>&1; then
+        echo "Need tool: $1"
+        exit 1;
+    fi
+}
+SLIC=""
+if [ "X$OS" == "XLinux" ] && [ "X$ARCH" == "Xx86_64" ]; then
+     checktool ldd
+     LDDVERSION=$(ldd --version|head -n 1)
+     if [ "X$LDDVERSION" == "Xldd (GNU libc) 2.5" ];then
+         SLIC="libc/2.5/"
+     fi
+fi
 PERLURL='https://raw.githubusercontent.com/MYDan/perl/master'
 INSTALLERDIR='/opt/mydan'
-
 for T in "Linux:x86_64" "Linux:i686" "CYGWIN_NT-6.1:x86_64" "FreeBSD:amd64" "FreeBSD:i386"
 do
     o=$(echo $T|awk -F: '{print $1}')
     a=$(echo $T|awk -F: '{print $2}')
     [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]&&  break
 done
-
 if [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]; then
     echo "OS:$OS ARCH:$ARCH ok"
 else
     echo "OS:$OS ARCH:$ARCH Not supported"
     exit 1
 fi
-
 if [ -f $INSTALLERDIR/perl/.lock ]; then
     echo "The perl is locked"
     exit 1;
 fi
-
-checktool() {
-    if ! type $1 >/dev/null 2>&1; then
-        echo "Need tool: $1"
-        exit 1;  
-    fi
-}
-
 checktool curl
 checktool wget
 checktool md5sum
-
 if [ -d "$INSTALLERDIR/perl" ]; then
     echo 'Already installed'
     exit 1
 fi
-
-VVVV=$(curl -k -s $PERLURL/data/$OS/$ARCH/version)
-
+VVVV=$(curl -k -s $PERLURL/data/$OS/$ARCH/$SLIC/version)
 version=$(echo $VVVV|awk -F: '{print $1}')
 md5=$(echo $VVVV|awk -F: '{print $2}')
-
 if [[ $version =~ ^[0-9]{14}$ ]];then
     echo "perl version: $version"
 else
     echo "get version fail"
     exit 1;
 fi
-
-
 clean_exit () {
     [ -f $LOCALINSTALLER ] && rm $LOCALINSTALLER
     echo  "ERROR"
     exit $1
 }
-
 get_repo ()
 {
     ALLREPO=$1
     C=${#ALLREPO[@]}
-
     DF=/tmp/x.$$.tmp
     mkfifo $DF
     exec 1000<>$DF
     rm -f $DF
-
     for((n=1;n<=$C;n++))
     do
         echo >&1000
     done
-
     for((i=0;i<$C;i++))
     do
         read -u1000
@@ -81,9 +74,7 @@ get_repo ()
             echo "$i:$s" >&1000
         }&
     done
-
     wait
-
     for((i=1;i<=$C;i++))
     do
         read -u1000 X
@@ -93,44 +84,30 @@ get_repo ()
             ID=$id
         fi
     done
-
     exec 1000>&-
     exec 1000<&-
-
     if [ "X$ID" != "X" ] && [ "X$ID" != "X0" ];then
         MYDan_REPO=${ALLREPO[$ID]}
     fi
 }
-
 ALLREPO=( https://raw.githubusercontent.com/MYDan/openapi/master http://180.153.186.60 http://223.166.174.60 )
 get_repo $ALLREPO
-
 if [ -z "$MYDan_REPO" ];then
-    PACKTAR=$PERLURL/data/$OS/$ARCH/perl.$version.tar.gz
+    PACKTAR=$PERLURL/data/$OS/$ARCH/${SLIC}perl.$version.tar.gz
 else
-    PACKTAR="$MYDan_REPO/perl/data/$OS/$ARCH/perl.$version.tar.gz"
+    PACKTAR="$MYDan_REPO/perl/data/$OS/$ARCH/${SLIC}perl.$version.tar.gz"
 fi
-
-
 LOCALINSTALLER=$(mktemp perl.XXXXXX)
-
 wget --no-check-certificate -O $LOCALINSTALLER "$PACKTAR" || clean_exit 1
-
 fmd5=$(md5sum $LOCALINSTALLER|awk '{print $1}')
-
 if [ "X$md5" != "X$fmd5" ];then
     echo "perl $version md5 nomatch"
     exit 1;
 fi
-
 if [ ! -e $INSTALLERDIR ]; then
     mkdir -p $INSTALLERDIR
 fi
-
 tar -zxf $LOCALINSTALLER -C $INSTALLERDIR || clean_exit 1
-
 [ -f $LOCALINSTALLER ] && rm $LOCALINSTALLER
-
 echo $version > $INSTALLERDIR/perl/.version
-
 echo OK
