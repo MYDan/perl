63f01de0c0c71132240bcae85ac1606a39aed977
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-12-30 14:14:12 +0800

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 OS=$(uname)
 ARCH=$(uname -m)
+
 checktool() {
     if ! type $1 >/dev/null 2>&1; then
         echo "Need tool: $1"
         exit 1;
     fi
 }
+
 SLIC=""
 if [ "X$OS" == "XLinux" ] && [ "X$ARCH" == "Xx86_64" ]; then
      checktool ldd
@@ -15,45 +17,55 @@ if [ "X$OS" == "XLinux" ] && [ "X$ARCH" == "Xx86_64" ]; then
          SLIC="libc/2.5/"
      fi
 fi
+
 PERLURL='https://raw.githubusercontent.com/MYDan/perl/master'
 INSTALLERDIR='/opt/mydan'
+
 for T in "Linux:x86_64" "Linux:i686" "CYGWIN_NT-6.1:x86_64" "FreeBSD:amd64" "FreeBSD:i386"
 do
     o=$(echo $T|awk -F: '{print $1}')
     a=$(echo $T|awk -F: '{print $2}')
     [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]&&  break
 done
+
 if [ "X$OS" == "X$o" ] && [ "X$ARCH" == "X$a" ]; then
     echo "OS:$OS ARCH:$ARCH ok"
 else
     echo "OS:$OS ARCH:$ARCH Not supported"
     exit 1
 fi
+
 if [ -f $INSTALLERDIR/perl/.lock ]; then
     echo "The perl is locked"
     exit 1;
 fi
+
 checktool curl
 checktool wget
 checktool md5sum
+
 if [ -d "$INSTALLERDIR/perl" ]; then
     echo 'Already installed'
     exit 1
 fi
+
 VVVV=$(curl -k -s $PERLURL/data/$OS/$ARCH/$SLIC/version)
 version=$(echo $VVVV|awk -F: '{print $1}')
 md5=$(echo $VVVV|awk -F: '{print $2}')
+
 if [[ $version =~ ^[0-9]{14}$ ]];then
     echo "perl version: $version"
 else
     echo "get version fail"
     exit 1;
 fi
+
 clean_exit () {
     [ -f $LOCALINSTALLER ] && rm $LOCALINSTALLER
     echo  "ERROR"
     exit $1
 }
+
 get_repo ()
 {
     ALLREPO=$1
@@ -90,6 +102,7 @@ get_repo ()
         MYDan_REPO=${ALLREPO[$ID]}
     fi
 }
+
 ALLREPO=( https://raw.githubusercontent.com/MYDan/openapi/master http://180.153.186.60 http://223.166.174.60 )
 get_repo $ALLREPO
 if [ -z "$MYDan_REPO" ];then
@@ -97,6 +110,7 @@ if [ -z "$MYDan_REPO" ];then
 else
     PACKTAR="$MYDan_REPO/perl/data/$OS/$ARCH/${SLIC}perl.$version.tar.gz"
 fi
+
 LOCALINSTALLER=$(mktemp perl.XXXXXX)
 wget --no-check-certificate -O $LOCALINSTALLER "$PACKTAR" || clean_exit 1
 fmd5=$(md5sum $LOCALINSTALLER|awk '{print $1}')
@@ -104,10 +118,13 @@ if [ "X$md5" != "X$fmd5" ];then
     echo "perl $version md5 nomatch"
     exit 1;
 fi
+
 if [ ! -e $INSTALLERDIR ]; then
     mkdir -p $INSTALLERDIR
 fi
+
 tar -zxf $LOCALINSTALLER -C $INSTALLERDIR || clean_exit 1
 [ -f $LOCALINSTALLER ] && rm $LOCALINSTALLER
 echo $version > $INSTALLERDIR/perl/.version
-echo OK
+
+echo perl install OK
